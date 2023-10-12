a3dcc0dd63263fb7665981e5a1a48e9ceca4912a
OmarElkeshen38 <oelkeshen5@gmail.com>
2023-10-12 21:59:33 +0300

    add user-orders component

--- src/Components/UserComp/UserOrders/UserOrders.jsx
+++ src/Components/UserComp/UserOrders/UserOrders.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react';
+import styles from './UserOrders.module.css';
+import Subtitle from '../../Utilities/Subtitle/Subtitle';
+import orderImg1 from "../../../images/catigory1.jpg";
+import orderImg2 from "../../../images/catigory2.jpg";
+import orderImg3 from "../../../images/catigory5.jpg";
 
 function UserOrders() {
+
+  const [orders, setOrders] = useState([
+    {id: 1, image: orderImg1, desc: "وصف المنتج دا انه جامد جدا ومفيش زيه وتوحفه بص انت يو شود تختاره حرفيا جامد جدا ومفيش زيه وتوحفه بص انت يو شود تختاره حرفيا", amount: 8},
+    {id: 2, image: orderImg2, desc: "وصف المنتج دا انه جامد جدا ومفيش زيه وتوحفه بص انت يو شود تختاره حرفيا جامد جدا ومفيش زيه وتوحفه بص انت يو شود تختاره حرفيا", amount: 5},
+    {id: 3, image: orderImg3, desc: "وصف المنتج دا انه جامد جدا ومفيش زيه وتوحفه بص انت يو شود تختاره حرفيا جامد جدا ومفيش زيه وتوحفه بص انت يو شود تختاره حرفيا", amount: 1},
+  ]);
+
+
   return (
     <>
-      <h1>User Orders</h1>
+      <Subtitle catig="مرحبا عمر مصطفى" />
+      <div className={`p-3 text-white rounded-2 ${styles.content}`}>
+        <div className="my-2">
+          <p className='fs-5'>الطلب رقم #21562498</p>
+          <div className="container">
+            <div className="row">
+              {orders.map((order) => (
+                <>
+                  <div className="col-lg-2 col-md-4 my-2">
+                  <img src={order.image} className="w-100" alt="" />
+                </div>
+                <div className="col-lg-10 col-md-8 my-2">
+                  <h4 className="fs-5">{order.desc}</h4>
+                  <h4 className='my-3'>الكمية : <span className='border px-3 fs-5 py-1'>{order.amount}</span></h4>
+                </div>
+              </>
+              ))}
+              
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-100 my-3 d-flex justify-content-center gap-3">
+        <button className='btn btn-info px-5 fs-5'>السابق</button>
+        <button className='btn btn-info px-5 fs-5'>التالي</button>
+      </div>
     </>
-  )
+  );
 }
 
 export default UserOrders
--- src/Pages/UserPage/UserPage.jsx
+++ src/Pages/UserPage/UserPage.jsx
@@ -7,7 +7,7 @@ function UserPage() {
     <>
       <div className="container">
             <div className="row">
-                <div className="col-lg-2 my-4">
+                <div className="col-lg-2 col-md-4 my-4">
                     <div className={`p-3 d-flex flex-column rounded-2 ${styles.bgLinks}`}>
                         <Link to={'user-orders'} className='text-white-50 fs-5 border text-center rounded-2 p-2 my-2'>ادارة الطلبات</Link>
                         <Link to={'fav-products'} className='text-white-50 fs-5 border text-center rounded-2 p-2 my-2'>المنتجات المفضلة</Link>
@@ -15,7 +15,7 @@ function UserPage() {
                         <Link to={'user-profile'} className='text-white-50 fs-5 border text-center rounded-2 p-2 my-2'>الملف الشخصي</Link>
                     </div>
                 </div>
-                <div className="col-lg-10 my-3">
+                <div className="col-lg-10 col-md-8 my-3">
                     <div className="p-3 rounded-2">
                         <Outlet />
                     </div>
